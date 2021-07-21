99a64da5770f466ee98b37ffdcb7adc1e90a4535
Marantral <infiltration.jedi@gmail.com>
2021-07-21 11:21:01 -0400

    Updated main py

--- subdom.py
+++ subdom.py
@@ -33,28 +33,28 @@ def wayback(sites):
 
 def scandns(sites):
     isp = {"23.202.231.169", "23.221.222.250"}
-    ddns = {'.herokudns.com', ".herokuapp.com", "herokussl.com", ".azurewebsites.net", ".cloudapp.net",
+    ddns = {'.herokudns.com', ".herokuapp.com", ".herokussl.com", ".azurewebsites.net", ".cloudapp.net",
             ".azure-api.net", ".trafficmanager.net", ".azureedge.net", ".cloudapp.azure.com", ".cloudfront.net",
-            ".s3.amazonaws.com", ".awsptr.com", ".elasticbeanstalk.com", ".uservoice.com", "unbouncepages.com",
-            "ghs.google.com", "ghs.googlehosted.com", ".ghs-ssl.googlehosted.com", ".github.io", "www.gitbooks.io",
-            "sendgrid.net", ".feedpress.me", ".fastly.net", ".webflow.io", "proxy.webflow.com", ".helpscoutdocs.com",
+            ".s3.amazonaws.com", ".awsptr.com", ".elasticbeanstalk.com", ".uservoice.com", ".unbouncepages.com",
+            ".ghs.google.com", ".ghs.googlehosted.com", ".ghs-ssl.googlehosted.com", ".github.io", ".www.gitbooks.io",
+            ".sendgrid.net", ".feedpress.me", ".fastly.net", ".webflow.io", ".proxy.webflow.com", ".helpscoutdocs.com",
             ".readmessl.com", ".desk.com", ".zendesk.com", ".mktoweb.com", ".wordpress.com", ".wpengine.com",
             ".cloudflare.net", ".netlify.com", ".bydiscourse.com", ".netdna-cdn.com", ".pageserve.co",
-            ".pantheonsite.io", ".arlo.co", ".apigee.net", ".pmail5.com", ".cm-hosting.com", "ext-cust.squarespace.com",
-            "ext.squarespace.com", "www.squarespace6.com", ".locationinsight.com", ".helpsite.io", "saas.moonami.com",
-            "custom.bnc.lt", ".qualtrics.com", ".dotcmscloud.net", ".dotcmscloud.com", ".knowledgeowl.com",
-            ".atlashost.eu", "headwayapp.co", "domain.pixieset.com", "cname.bitly.com", ".awmdm.com", ".meteor.com",
-            ".postaffiliatepro.com", "na.iso.postaffiliatepro.com", ".copiny.com", ".kxcdn.com", "phs.getpostman.com",
+            ".pantheonsite.io", ".arlo.co", ".apigee.net", ".pmail5.com", ".cm-hosting.com", ".ext-cust.squarespace.com",
+            ".ext.squarespace.com", ".www.squarespace6.com", ".locationinsight.com", ".helpsite.io", ".saas.moonami.com",
+            ".custom.bnc.lt", ".qualtrics.com", ".dotcmscloud.net", ".dotcmscloud.com", ".knowledgeowl.com",
+            ".atlashost.eu", "headwayapp.co", ".domain.pixieset.com", ".cname.bitly.com", ".awmdm.com", ".meteor.com",
+            ".postaffiliatepro.com", ".na.iso.postaffiliatepro.com", ".copiny.com", ".kxcdn.com", ".phs.getpostman.com",
             ".appdirect.com", ".streamshark.io", ".ethosce.com", ".custhelp.com", ".onelink-translations.com",
-            ".mashery.com", ".edgesuite.net", ".akadns.net", ".edgekey.net", 'akamaiedge.net', ".edgekey-staging.net",
+            ".mashery.com", ".edgesuite.net", ".akadns.net", ".edgekey.net", '.akamaiedge.net', ".edgekey-staging.net",
             ".lldns.net", ".edgecastcdn.net", "centercode.com", ".jivesoftware.com", ".cvent.com", ".covisint.com",
             ".digitalrivercontent.net", ".akahost.net", ".connectedcommunity.org", ".lithium.com", ".sl.smartling.com",
-            "pfsweb.com", ".bsd.net", ".vovici.net", ".extole.com", ".ent-sessionm.com", ".eloqua.com", ".inscname.net",
-            "insnw.net", ".2o7.net", ".wnmh.net", ".footprint.net", ".llnwd.net", ".cust.socrata.net", ".scrool.se",
+            ".pfsweb.com", ".bsd.net", ".vovici.net", ".extole.com", ".ent-sessionm.com", ".eloqua.com", ".inscname.net",
+            ".insnw.net", ".2o7.net", ".wnmh.net", ".footprint.net", ".llnwd.net", ".cust.socrata.net", ".scrool.se",
             ".phenompeople.com", ".investis.com", ".skilljar.com", ".imomentous.com", ".cleverbridge.com", ".insnw.net",
-            "sailthru.com", "static.captora.com", ".q4web.com", ".omtrdc.net", ".devzing.com", ".pphosted.com",
+            ".sailthru.com", ".static.captora.com", ".q4web.com", ".omtrdc.net", ".devzing.com", ".pphosted.com",
             ".securepromotion.com", ".getbynder.com", ".certain.com", ".certainaws.com", ".eds.com", ".bluetie.com",
-            ".relayware.com", ".yodlee.com", ".mrooms.net", "ssl.cdntwrk.com", "secure.gooddata.com", ".deltacdn.net",
+            ".relayware.com", ".yodlee.com", ".mrooms.net", ".ssl.cdntwrk.com", ".secure.gooddata.com", ".deltacdn.net",
             ".happyfox.com", ".proformaprostores.com", ".yext-cdn.com", ".edgecastdns.net", ".ecdns.net"}
     q = dns.resolver.resolve(sites, 'A')
     for rname in q:
@@ -62,24 +62,27 @@ def scandns(sites):
         if name in isp:
             pass
         else:
-            print("[+] Subdomain:" + sites + " : IP being: " + name + "\n")
-            up = "echo 'Subdomain Found!!:  " + sites + " with the IP of: " + name + r" \n ' >> ./" + domain + "_subdomain_Scan.txt"
-            subprocess.call(up, shell=True)
-            if sites not in site2:
-                current = sites[:]
-                site2.append(current)
-
-            q = dns.resolver.resolve(sites, 'CNAME')
-            for bong in q:
-                c_val = str(bong.target)
-                print("\n[+] The CNAME for " + sites + " is: " + c_val)
-                inputfile = "echo '  CNAME Results for " + sites + " is:  " + c_val + r" \n' >> ./" + domain + "_subdomain_Scan.txt"
-                subprocess.call(inputfile, shell=True)
-                for d in ddns:
-                    if d in c_val:
-                        print(ERROR + "\n\t!!!!!we might have a dangler!!!!! \n\t" + c_val + " : " + d + " : Subdomain: " + sites + "\n\n" + ENDC)
-                        inputfile = "echo '  CNAME could be vulnerable to dangling DNS " + sites + " is:  " + c_val + " Which is connected to known Dangling DNS source: " + d + r"  you should check on that! \n' >> ./" + domain + "_subdomain_Scan.txt "
+            with open(domain + "_subdomain_Scan.txt") as file:
+                if sites in file.read():
+                    pass
+                else:
+                    print("[+] Subdomain:" + sites + " : IP being: " + name + "\n")
+                    up = "echo 'Subdomain Found!!:  " + sites + " with the IP of: " + name + r" \n ' >> ./" + domain + "_subdomain_Scan.txt"
+                    subprocess.call(up, shell=True)
+                    if sites not in site2:
+                        current = sites[:]
+                        site2.append(current)
+                    q = dns.resolver.resolve(sites, 'CNAME')
+                    for bong in q:
+                        c_val = str(bong.target)
+                        print(GREEN + "\n[+] The CNAME for " + sites + " is: " + c_val + ENDC)
+                        inputfile = "echo '  CNAME Results for " + sites + " is:  " + c_val + r" \n' >> ./" + domain + "_subdomain_Scan.txt"
                         subprocess.call(inputfile, shell=True)
+                        for d in ddns:
+                            if d in c_val:
+                                print(ERROR + "\n\t!!!!!we might have a dangler!!!!! \n\t" + c_val + " : " + d + " : Subdomain: " + sites + "\n\n" + ENDC)
+                                inputfile = "echo '  CNAME could be vulnerable to dangling DNS " + sites + " is:  " + c_val + " Which is connected to known Dangling DNS source: " + d + r"  you should check on that! \n' >> ./" + domain + "_subdomain_Scan.txt "
+                                subprocess.call(inputfile, shell=True)
 
 
 
@@ -117,7 +120,8 @@ def main():
                                       Version 0.2 
     """ + ENDC)
 
-
+    print(BLUE + "\n\tWhen you put in the domain to be scanned use the parent domain!")
+    print("\tLike: 'google.com' or 'company.org'\n\n" + ENDC)
 
     domain = input("what is the domain to be scanned?: ").strip()
 
@@ -142,13 +146,32 @@ def main():
     with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
         executor.map(wayback, site2)
 
-    print(ERROR + "\n\tWe are looking for interesting urls within the Wayback for " + domain + "\n" + ENDC)
+    print(GREEN + "\n\t-----We are looking for interesting urls within the Wayback for " + domain + "-----\n" + ENDC)
     site_1 = "https://web.archive.org/cdx/search/cdx?url=*." + domain + "/*&output=text&fl=original&collapse=urlkey"
     response_2 = requests.get(site_1)
     response_fin = response_2.text
     file = open(domain + "_way_osint.txt", "w")
     file.write(response_fin)
     file.close()
+
+    pars = ["user", "pass", "password", "pword", "username", "USER", "Pass", "token", "UserName", "Token", "secret",
+            "email", "Password", "User", "admin", "Administrator", "ADMIN", "Admin", "jsession", "userid", ]
+
+    risk = []
+    for p in pars:
+        if p in response_fin:
+            if p not in risk:
+                current = p[:]
+                risk.append(current)
+
+    for r in risk:
+        print(BLUE + "\n\tPotential interesting parameter found!: " + BOLD + ERROR + r + ENDC)
+
+    print(BLUE + "\n\tThere might be interesting things within the parameters. However, we might not found them "
+                         "automaticly.\n\tTake a look!!" + ENDC)
+
+    print("\n\tOutput files are: \n\t" + GREEN + domain + "_subdomain_Scan.txt \n\t" + domain
+          + "_wayback_scan.txt \n\t" + domain + "_way_osint.txt\n" + ENDC)
     print("Thanks for using SubDominal!")
 
 # call main() function
